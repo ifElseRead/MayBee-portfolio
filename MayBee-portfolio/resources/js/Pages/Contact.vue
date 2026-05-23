5683213675611eb7863663cb55bb636b3f20e883
ifElseRead <johnmurphy9@gmail.com>
2026-05-23 01:41:29 +0100

    updated email on contact form

--- MayBee-portfolio/resources/js/Pages/Contact.vue
+++ MayBee-portfolio/resources/js/Pages/Contact.vue
@@ -331,7 +331,7 @@ const submit = async () => {
                             >
                                 <div class="text-sm text-slate-500">Email</div>
                                 <div class="mt-1 font-bold text-slate-900">
-                                    Use the form above.
+                                    Use the form above...
                                 </div>
                             </a>
 
