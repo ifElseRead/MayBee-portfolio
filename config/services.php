4e7b8aac28457aa1c222130462dd00ab28ad7711
ifElseRead <johnmurphy9@gmail.com>
2026-05-24 17:05:50 +0100

    fix: mailgun service not registered

--- config/services.php
+++ config/services.php
@@ -43,6 +43,13 @@ return [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+
+    ],
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
     ],
 
 ];
